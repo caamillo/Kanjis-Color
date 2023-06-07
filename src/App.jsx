cc02df1d3dfc805e6d34ad0295c453210b6eb683
caamillo <mcamillolud@gmail.com>
2023-06-08 01:24:02 +0200

    Btn Lil Anim

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import ColorPicker from 'react-best-gradient-color-picker'
 
 function App() {
   const [open, setOpen] = useState(false)
+  const [isClicked, setIsClicked] = useState(false)
   const [color, setColor] = useState('rgba(255,0,0,1)')
 
   return (
     <div className="w-screen h-screen bg-[#353734] flex flex-col justify-center relative items-center">
-      <button className='bg-white mb-5 px-3 py-2 rounded-md' onClick={ () => setOpen(tempOpen => !tempOpen) }>Change Color</button>
+      <button style={isClicked ? { transform: 'scale(110%)' } : {}} onMouseDown={ () => setIsClicked(true) } onMouseUp={ () => setIsClicked(false) } className='bg-white transition-transform hover:scale-95 mb-5 px-3 py-2 rounded-md' onClick={ () => setOpen(tempOpen => !tempOpen) }>Change Color</button>
       <div className='relative w-[12vw] max-w-[185px] min-w-[130px]'>
         <img src={ outlineAsset } className='w-full z-30 relative'/>
         <div style={{ background: color }} className='w-full h-full z-10 absolute top-0'></div>
